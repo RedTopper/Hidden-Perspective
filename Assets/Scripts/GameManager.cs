141ff460b286540c9bfa31b7968b9d422a12d3ff
Andrew Peters <adpeters5@gmail.com>
2018-10-20 01:37:51 -0500

    Created a random line generator i guess.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BeginGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+    }
+
+    public Maze mazePrefab;
+
+    private Maze mazeInstance;
+
+    private void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        StartCoroutine(mazeInstance.Generate());
+    }
+
+    private void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
---
+++ Assets/Scripts/MazeCell.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCell : MonoBehaviour {
+
+    public IntVector2 coordinates;
+
+}
