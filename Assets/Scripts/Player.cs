7ad9b7f370c393c7396c7709a4081c6293d10a64
keyes <keyes.anderson@gmail.com>
2018-10-20 00:55:56 -0500

    Player commit 1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int xMom;
+    private int yMom;
+    private int zMom;
+    private bool onGround;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 player = this.transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.z++;
+            this.transform.position = player;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.x--;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.z--;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.x++;
+            this.transform.position = position;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && onGround)
+        {
+            player.y++;
+            this.transform.position = position;
+        }
+    }
+}
