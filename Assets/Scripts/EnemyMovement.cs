5e621ede634110991a06a897ea1f1eaac45520f6
Andrew Peters <adpeters5@gmail.com>
2018-10-21 00:34:00 -0500

    hopefully move man

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;               // Reference to the player's position.
+    NavMeshAgent nav;
+
+
+    void Awake()
+    {
+        // Set up the references.
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        // ... set the destination of the nav mesh agent to the player.
+        //Maze maze = GameObject.Find("Maze Runner").GetComponent<Maze>();
+        nav.SetDestination(player.position);
+       
+    }
+}
