6e2a015a3f9b06cbb37d0d265c9d1d4f92a4e97c
keyes <keyes.anderson@gmail.com>
2018-10-20 20:07:45 -0500

    added archs

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -17,10 +17,10 @@ public class Maze : MonoBehaviour {
 
     public MazeWall wallPrefab;
 
-    public MazeDoor doorPrefab;
+    public MazeArch archPrefab;
 
     [Range(0f, 1f)]
-    public float doorProb;
+    public float archProb;
 
     public IntVector2 RandomCoordinates
     {
@@ -92,10 +92,10 @@ public class Maze : MonoBehaviour {
 
     private void CreatePassage(MazeCell cell, MazeCell otherCell, MazeDirection direction)
     {
-        MazePassage prefab = Random.value < doorProb ? doorPrefab : passagePrefab;
-        MazePassage passage = Instantiate(passagePrefab) as MazePassage;
+        MazePassage prefab = Random.value < archProb ? archPrefab : passagePrefab;
+        MazePassage passage = Instantiate(prefab) as MazePassage;
         passage.Initialize(cell, otherCell, direction);
-        passage = Instantiate(passagePrefab) as MazePassage;
+        passage = Instantiate(prefab) as MazePassage;
         passage.Initialize(otherCell, cell, direction.GetOpposite());
     }
 
