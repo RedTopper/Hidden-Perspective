895fb6f0e774187e94483dff96da03ccea34852e
keyes <keyes.anderson@gmail.com>
2018-10-21 01:38:25 -0500

    added main menu

---
+++ Assets/MainManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            toGame(1);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            toQuit();
+    }
+
+	public void toGame(int scene)
+    {
+        Application.LoadLevel(scene);
+    }
+	
+	public void toQuit()
+    {
+        Application.Quit();
+    }
+}
